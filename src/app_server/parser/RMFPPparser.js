d72581daa42b7c0e0a69d098335883e66a1de697
Justin Santer <jsanter@vicominfinity.com>
2021-08-04 10:44:32 -0400

    Proposed changes to RMFPP CPU and WLGML JSON formats
    Signed-off-by: Justin Santer <jsanter@vicominfinity.com>

--- src/app_server/parser/RMFPPparser.js
+++ src/app_server/parser/RMFPPparser.js
@@ -11,70 +11,79 @@ module.exports.bodyParserforRmfCPUPP = function (data, fn) {//Function to parse 
     try { //try to process xml
         parser.parseString(data, function (err, result) {
             var finalJSON = []; // Collection for storing JSON of Parsed CPU XML
-            var key1 = result['ddsml']['postprocessor']; // keys1 represent all postprocessor report sections
-            //For loop to create dictionary/JSON 
-            for (k in key1) { //looping through all postprocessor sections, one at a time: k is an integer startinf from zero and increase with each iteration
-                var cpuMachineType = key1[k]['segment'][0]['part'][0]['var-list'][0]['var'][0]['value'][0];
-                var cpuModelType = key1[k]['segment'][0]['part'][0]['var-list'][0]['var'][1]['value'][0];
-                var CPUSectiontable = key1[k]['segment'][0]['part'][0]['table'][0]["row"] // Rows in CPU Section of XML
-                var CPUsectioncolumnheader = key1[k]['segment'][0]['part'][0]['table'][0]['column-headers'][0]['col']; // Column headers in CPU Section of XML
-                var numProcName = key1[k]['segment'][1]['part'][1]['name'][0]; // Number of Physical Processors section name
-                var numProcTable = key1[k]['segment'][1]['part'][1]['table'][0]['row']; // Table of Number of Physical Processors section
-                var partitionDataName = key1[k]['segment'][1]['part'][2]['name'][0]; // Partion data section name
-                var partitionDataTable = key1[k]['segment'][1]['part'][2]['table'][0]['row']; // Table of the partition data section
-                var partitionDataColumnHead = key1[k]['segment'][1]['part'][2]['table'][0]['column-headers'][0]['col']; //Columnhead of the partition data section
-                var resourceName = key1[k]['resource'][0]['resname'][0];
-                var resourceType = key1[k]['resource'][0]['restype'][0];
-                var PDCH = [] //partition data columnheader collection
-                var FPDR = [] //final partition data report collection 
-                for (i in partitionDataColumnHead) { //looping through partitionDataColumnHead
-                    PDCH[i] = partitionDataColumnHead[i]['_']; //populating the partition data columnheader collection
-                };
+            var postprocessors = result['ddsml']['postprocessor']; // keys1 represent all postprocessor report sections
+            for (a in postprocessors) {
+                var singleCPUReport = {};
+                var segments = postprocessors[a]['segment'];
+                var resourceName = postprocessors[a]['resource'][0]['resname'][0];
+                var allSegmentCollection = {};
+                for (b in segments) {
+                    var parts = segments[b]['part'];
+                    var segmentName = segments[b]['name'][0];
+                    var segmentCollection = {};
+                    var partCollection = {};
+                    for (c in parts) {
+                        var partName = parts[c]['name'];
+                        var varlist = parts[c]['var-list'];
+                        var table = parts[c]['table'];
+                        var fieldCollection = {};
 
-                for (i in partitionDataTable) {   //looping through partitionDatatable
-                    var PDTB = {} //Partition data Table body collection
-                    for (j in PDCH) { //looping through partition Data ColumnHead
-                        PDTB[PDCH[j]] = partitionDataTable[i].col[j]; //creating a key value pairs for each lpar in the partion data table  
+                        if (varlist) {
+                            var variables = varlist[0]['var'];
+                            for (d in variables) {
+                                fieldCollection[variables[d]['name'][0]] = variables[d]['value'][0];
+                            }
+                        }
+
+                        if (table) {
+                            var tableColumnHeader = table[0]['column-headers'][0]['col'];
+                            var tableBody = table[0]['row'];
+                            var columnheadCollection = [];
+                            var finalTableReport = [];
+                            for (i in tableColumnHeader) {
+                                columnheadCollection[i] = tableColumnHeader[i]['_'];
+                            }
+
+                            if (tableBody !== undefined) {
+                                for ( i in tableBody) {
+                                    var CPUtable = {}
+                                    for (j in columnheadCollection) {
+                                        CPUtable[columnheadCollection[j]] = tableBody[i]['col'][j];
+                                    }
+                                    finalTableReport.push(CPUtable);
+                                }
+                            }
+
+                            if (!varlist) {
+                                fieldCollection = finalTableReport;
+                            } else {   
+                                fieldCollection["Table"] = finalTableReport;
+                            }
+                        }
+
+                        if (partName) {
+                            partCollection[partName] = fieldCollection;
+                        } else {
+                            partCollection['Info'] = fieldCollection;
+                        }
+                        if (segmentCollection) {
+                            allSegmentCollection[segmentName] = partCollection;
+                        } else {
+                            allSegmentCollection['Segment'] = partCollection;
+                        }
                     }
-                    FPDR.push(PDTB); //populating final partition data report collection
-                };
-
-                var FNPT = {} // final number of phys proc table
-                var regExp = /\(([^)]+)\)/; // used to get shorthand proc name
-                for (var i = 0; i < numProcTable.length - 1; i++) {
-                    var matches = regExp.exec(numProcTable[i]['col'][0])
-                    FNPT[matches[1]] = numProcTable[i]['col'][1]
                 }
-
-                CPUColumnhead = [] // CPU columnheader collection
-                for (i in CPUsectioncolumnheader) { //looping through CPUsectioncolumnheader
-                    CPUColumnhead[i] = CPUsectioncolumnheader[i]['_']; //populating the CPU columnheader collection
+                singleCPUReport['Report'] = postprocessors[a]['metric'][0]["description"][0];
+                singleCPUReport["System"] = resourceName;
+                singleCPUReport['Timestamp'] = postprocessors[a]['time-data'][0]['display-start'][0]['_'];
+                singleCPUReport = {
+                    ...singleCPUReport,
+                    ...allSegmentCollection
                 };
 
-                finalCPUTable = [];//final CPU report collection
-                for (i in CPUSectiontable) {   //looping through CPUsectiontable
-                    CPUtable = {} //CPU table data collection
-                    for (j in CPUColumnhead) {//looping through CPU ColumnHead
-                        CPUtable[CPUColumnhead[j]] = CPUSectiontable[i].col[j]; //creating a key value pairs for each CPU in the CPU table  
-                    }
-                    finalCPUTable.push(CPUtable); //populating final CPU report collection
-                };
-
-                var finalCPUReport = { Machine: cpuMachineType, Model: cpuModelType, Table: finalCPUTable };
-
-                parsedPostprocessor = {}; //collection  for individual parsed postprocessor, one at a time
-                parsedPostprocessor['Report'] = key1[k]['metric'][0]["description"][0]; // Report key value pair
-                parsedPostprocessor['Timestamp'] = key1[k]['time-data'][0]['display-start'][0]['_']; // Timestamp key value pair
-                parsedPostprocessor[resourceType] = resourceName;
-                parsedPostprocessor['CPU'] = finalCPUReport; // CPU key value pair
-                parsedPostprocessor[numProcName] = FNPT;
-                parsedPostprocessor[partitionDataName] = FPDR;
-                //parsedPostprocessor['partitionDataName'] = partitionDataName; // partitionDataName key value pair
-                //parsedPostprocessor['partitionDataBody'] = FPDR; // partionDataBody key value pair
-
-                finalJSON.push(parsedPostprocessor); //populating FinalJSON with parsedPostprocessor
+                finalJSON.push(singleCPUReport);
             }
-            fn(finalJSON); //function returns parsed postprocessor
+            fn(finalJSON);
         });
     } catch (err) {// if parsing XML didn't went smooth
         fn({msg: 'Err', error: err, data: data});// return the error 
@@ -94,11 +103,12 @@ module.exports.bodyParserforRmfWLMPP = function (data, fn) {//Function to parse 
             for (a in postprocessors) { // loop through all postprocessor sections
                 var singleWLMPPReport = {}; // JSON Collection for a single postprocession section of the XML
                 var segments = postprocessors[a]['segment']; // represent segments each postprocessor section
+                var resourceName = postprocessors[a]['resource'][0]['resname'][0];
                 AllsegmentCollection = []; // A JSON collection for all segments
                 for (b in segments) { // loop through segments in the XML
                     var parts = segments[b]['part']; // represent segment part value in the XML
                     var message = segments[b]['message']; // represent segment message value in the XML
-                    var segmentName = segments[b]['name']; // represent segment name in the XML
+                    var segmentName = segments[b]['name'][0]; // represent segment name in the XML
                     segmentCollection = {}; //A JSON for single XML segment
                     if (message) {  // if segment contains mesaage atrributes in the XML
                         var messageDescription = message[0]['description'][0]; // represent message description
@@ -141,7 +151,12 @@ module.exports.bodyParserforRmfWLMPP = function (data, fn) {//Function to parse 
                                     finaltableReport.push(WLMtable);
                                 };
                             }
-                            fieldCollection["Table"] = finaltableReport;
+
+                            if (!varlist) {
+                                fieldCollection = finaltableReport;
+                            } else {
+                                fieldCollection["Table"] = finaltableReport;
+                            }
                         }
                         // Add to part
                         if (partName) { // if part name atrribute is present in the XML
@@ -150,17 +165,26 @@ module.exports.bodyParserforRmfWLMPP = function (data, fn) {//Function to parse 
                             partCollection['Info'] = fieldCollection; //populate part collections with "info" as key and finaltableReport as value
                         }
                         if (segmentName) { // if segment name atrribute is present in the XML
-                            segmentCollection[segmentName] = partCollection; //populate segment collections with segmentname as key and partCollection as value
+                            segmentCollection = {
+                                'Name': segmentName,
+                                ...segmentCollection,
+                                ...partCollection
+                            }
                         } else { // if segment name atrribute is not present in the XML
-                            segmentCollection['Segment'] = partCollection; //populate segment collections with "Info" as key and partCollection as value
+                            segmentCollection = {
+                                'Name': 'N/A',
+                                ...segmentCollection,
+                                ...partCollection
+                            }
                         }
 
                     }
                     AllsegmentCollection.push(segmentCollection) //Push segment collection into all segments
                 }
                 singleWLMPPReport['Report'] = postprocessors[a]['metric'][0]["description"][0]; //singleWLMPPReport report key value pair
+                singleWLMPPReport['Sysplex'] = resourceName;
                 singleWLMPPReport['Timestamp'] = postprocessors[a]['time-data'][0]['display-start'][0]['_']; //singleWLMPPReport Timestamp key value pair
-                singleWLMPPReport['ReportSegments'] = AllsegmentCollection; //singleWLMPPReport reportSegments key value pairs
+                singleWLMPPReport['Classes'] = AllsegmentCollection; //singleWLMPPReport reportSegments key value pairs
 
                 //console.log(ppReport);
                 allWLMPPJSON.push(singleWLMPPReport);  //push single workload postprocessor JSON into All workload post processor JSON
